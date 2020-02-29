2035b7f2645584fec7f2a2122a9b6c66d239b88c
Erfan Kashani <erfan.kashani@bell.ca>
2020-02-29 09:40:03 -0500

    Updates

---
+++ precache-manifest.3bc68ef1661524122873f18b98d41274.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d34527578718220fde85e2f5e4efd68",
+    "url": "/facerecognition-website/index.html"
+  },
+  {
+    "revision": "1a7d38707e2e4de8d9ac",
+    "url": "/facerecognition-website/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "2a3cf11353c6288efdde",
+    "url": "/facerecognition-website/static/css/main.cd9f545f.chunk.css"
+  },
+  {
+    "revision": "1a7d38707e2e4de8d9ac",
+    "url": "/facerecognition-website/static/js/2.87327a24.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/facerecognition-website/static/js/2.87327a24.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2a3cf11353c6288efdde",
+    "url": "/facerecognition-website/static/js/main.653bbf7b.chunk.js"
+  },
+  {
+    "revision": "ccc0e65a00ba7a93bb54",
+    "url": "/facerecognition-website/static/js/runtime-main.ae630539.js"
+  }
+]);
