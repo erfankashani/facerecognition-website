84afc08ca35c736482980e866b7da29e1ad0dd0d
Erfan Kashani <erfan.kashani@bell.ca>
2020-02-29 09:53:18 -0500

    try https

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,10 +1,6 @@
 import React from 'react';
 
 const Rank = ( props ) => {
-    // console.log(JSON.stringify(props.userInfo, null, 2));
-    // console.log(props.userInfo.name);
-    // console.log(props.userInfo.entries);
-    // const { entries , name} = userInfo;
     return(
         <div>
             <div className='white f3'>
